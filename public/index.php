6ef54704e7ee60efeb4307b245fb044567e457eb
Olav Lindekleiv <olav@lindekleiv.com>
2019-10-05 17:51:27 +0200

    Remove state from image field and split up list field component. Still some issues with reordering blocks with croppable images. Works, but preview gets messed up.

--- public/index.php
+++ public/index.php
@@ -2,10 +2,6 @@
 
 require_once '../vendor/autoload.php';
 
-if (preg_match('/^(\/(static|admin|uploads)(\/|$))/', $_SERVER['REQUEST_URI'])) {
-    return false;
-}
-
 use Babble\Babble;
 $babble = new Babble();
 $babble->serve();
